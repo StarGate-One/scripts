23b6a0ae049cfcbb1aa428c92963e211c38ec329
General Jack O'Neill <68293825+StarGate-One@users.noreply.github.com>
2021-08-12 09:59:57 -0400

    Modify scripts to move vcpkg-tool out of vcpkg vcpkg-tool now has an independent repository.

--- vcpkg-clone.cmd
+++ vcpkg-clone.cmd
@@ -1,7 +1,6 @@
 @echo off
 setlocal
 
-set COPYCMD=/b /v /y
 set _root_drive=D:
 set _vcpkg_root=%_root_drive%\vcpkg
 set _vcpkg_archives=%_vcpkg_root%\archives
@@ -12,11 +11,8 @@ set _vcpkg_installed=%_vcpkg_root%\installed
 set _vcpkg_packages=%_vcpkg_root%\packages
 set _vcpkg_metrics=%_vcpkg_root%\vcpkg.disable-metrics
 set _vcpkg_temp=%_vcpkg_root%\temp
-set _vcpkg_tool=%_vcpkg_root%\vcpkg-tool
-set _vcpkg_tool_module=vcpkg-tool
+set _vcpkg_tool=%_root_drive%\vcpkg-tool
 set _vcpkg_tool_build=%_vcpkg_tool%\build
-set _vcpkg_tool_source=%_vcpkg_tool%
-set _vcpkg_tool_exe=%_vcpkg_tool%\build\Release\vcpkg.exe
 
 %_root_drive%
 cd %_root_drive%\
@@ -25,39 +21,19 @@ if exist %_vcpkg_root% (
 	rmdir /q /s %_vcpkg_root%
 )
 
-rem git clone -c core.symlinks=false --verbose --progress --recursive git@github.com:microsoft/vcpkg.git %_vcpkg_root%
+if exist %_vcpkg_tool% (
+	rmdir /q /s %_vcpkg_tool%
+)
+
 git clone -c core.symlinks=false --verbose --progress --recursive git@github.com:StarGate-One/vcpkg.git %_vcpkg_root%
 
 cd %_vcpkg_root%
-rem git submodule update --init --recursive
 
 git remote add upstream git@github.com:microsoft/vcpkg.git
 
 if not exist %_vcpkg_metrics% (
 	echo. >%_vcpkg_metrics%
 )
-
-rem git clone -c core.symlinks=false --verbose --progress --recursive git@github.com:microsoft/vcpkg-tool.git %_vcpkg_tool%
-git clone -c core.symlinks=false --verbose --progress --recursive git@github.com:StarGate-One/vcpkg-tool.git %_vcpkg_tool%
-
-cd %_vcpkg_tool%
-rem git submodule update --init --recursive
-
-git remote add upstream git@github.com:microsoft/vcpkg-tool.git
-
-cmake -B %_vcpkg_tool_build% -S %_vcpkg_tool_source% -G "Visual Studio 16 2019" -A x64 -T v142 -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DVCPKG_DEVELOPMENT_WARNINGS=ON -DVCPKG_WARNINGS_AS_ERRORS=ON -DVCPKG_BUILD_FUZZING=OFF -DVCPKG_EMBED_GIT_SHA=ON -DVCPKG_ADD_SOURCELINK=ON
-
-cmake --build %_vcpkg_tool_build% --target vcpkg --clean-first --verbose --config release --parallel 8
-
-if exist %_vcpkg_tool_exe% (
-	copy %_vcpkg_tool_exe% %_vcpkg_exe%
-	cd %_vcpkg_root%
-	%_vcpkg_exe% version
-)
-
-cd %_vcpkg_root%
-rem git submodule add git@github.com:microsoft/vcpkg-tool.git %_vcpkg_tool%
-git submodule add git@github.com:StarGate-One/vcpkg-tool.git %_vcpkg_tool_module%
 
 if not exist %_vcpkg_archives% (
 	mkdir %_vcpkg_archives%
@@ -83,11 +59,12 @@ if not exist %_vcpkg_temp% (
 	mkdir %_vcpkg_temp%
 )
 
+cd %_root_drive%\
+
+git clone -c core.symlinks=false --verbose --progress --recursive git@github.com:StarGate-One/vcpkg-tool.git %_vcpkg_tool%
+
+cd %_vcpkg_tool%
+
+git remote add upstream git@github.com:microsoft/vcpkg-tool.git
+
 endlocal
-rem git submodule update --init --recursive -v vcpkg-tool
-rem $ cat .gitmodules
-rem [submodule "vcpkg-tool"]
-rem        path = vcpkg-tool
-rem        url = git@github.com:StarGate-One/vcpkg-tool.git
-rem        fetchRecurseSubmodules = on-demand
-rem        branch = .
