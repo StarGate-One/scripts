e00ef59a69a4062132d0dc350a71007fd5285591
General Jack O'Neill <68293825+StarGate-One@users.noreply.github.com>
2021-09-01 20:55:45 -0400

    Add new repos

--- Add_To_Github.cmd
+++ Add_To_Github.cmd
@@ -2,57 +2,81 @@
 cd D:\Projects\Git
 
 cd _vcpkg
+git branch -M main
 git remote add origin git@github.com:StarGate-One/_vcpkg.git
 git push -u origin main
 
 cd ../Allegro5
+git branch -M main
 git remote add origin git@github.com:StarGate-One/Allegro5.git
 git push -u origin main
 
 cd ../Bible
+git branch -M main
 git remote add origin git@github.com:StarGate-One/Bible.git
 git push -u origin main
 
 cd ../boost
+git branch -M main
 git remote add origin git@github.com:StarGate-One/boost.git
 git push -u origin main
 
 cd ../C_Cpp
+git branch -M main
 git remote add origin git@github.com:StarGate-One/C_Cpp.git
 git push -u origin main
 
 cd ../glfw_3.3.4
+git branch -M main
 git remote add origin git@github.com:StarGate-One/glfw_3.3.4.git
 git push -u origin main
 
 cd ../MFC
+git branch -M main
 git remote add origin git@github.com:StarGate-One/MFC.git
 git push -u origin main
 
 cd ../Ogre
+git branch -M main
 git remote add origin git@github.com:StarGate-One/Ogre.git
 git push -u origin main
 
 cd ../OpenGL
+git branch -M main
 git remote add origin git@github.com:StarGate-One/OpenGL.git
 git push -u origin main
 
 cd ../SDL2
+git branch -M main
 git remote add origin git@github.com:StarGate-One/SDL2.git
 git push -u origin main
 
 cd ../SFML
+git branch -M main
 git remote add origin git@github.com:StarGate-One/SFML.git
 git push -u origin main
 
 cd ../vcpkg_x64
+git branch -M main
 git remote add origin git@github.com:StarGate-One/vcpkg_x64.git
 git push -u origin main
 
 cd ../vcpkgMgr
+git branch -M main
 git remote add origin git@github.com:StarGate-One/vcpkgMgr.git
 git push -u origin main
 
 cd ../scripts
+git branch -M main
 git remote add origin git@github.com:StarGate-One/scripts.git
+git push -u origin main
+
+cd ../gnucobol-3.2-dev_win.git
+git branch -M main
+git remote add origin git@github.com:StarGate-One/gnucobol-3.2-dev_win.git
+git push -u origin main
+
+cd ../vbisam-2.0.1
+git branch -M main
+git remote add origin git@github.com:StarGate-One/vbisam-2.0.1
 git push -u origin main
