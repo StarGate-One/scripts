955abdfea71579759928b52ed3346f84da346e52
General Jack O'Neill <68293825+StarGate-One@users.noreply.github.com>
2021-12-03 07:52:00 -0500

    Add Actual last commit Date/Time and UTC offset to program version

--- vcpkg-tool.cmd
+++ vcpkg-tool.cmd
@@ -13,6 +13,18 @@ set _vcpkg_tool_exe=%_vcpkg_tool%\build\Release\vcpkg.exe
 
 call date-time.cmd
 
+set _vcpkg-tool_latest_git-commit-date=EMPTY
+
+set _vcpkg_git_format="--format=%%cd"
+set _vcpkg_git_date_format="--date=iso-strict"
+
+for /f "tokens=* USEBACKQ" %%g in (`git log -n 1 %_vcpkg_git_format% %_vcpkg_git_date_format%`) do (
+	set _vcpkg-tool_latest_git-commit-date=%%g
+	if %_vcpkg-tool_latest_git-commit-date% == "EMPTY" (
+       set _vcpkg-tool_latest_git-commit-date=%file_current-date%
+	)
+)
+
 %_root_drive%
 cd %_root_drive%\
 
@@ -23,7 +35,7 @@ if exist %_vcpkg_tool_build% (
    mkdir %_vcpkg_tool_build%
 )
 
-cmake -B %_vcpkg_tool_build% -S %_vcpkg_tool_source% -G "Visual Studio 16 2019" -A x64 -T v142 -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DVCPKG_DEVELOPMENT_WARNINGS=ON -DVCPKG_WARNINGS_AS_ERRORS=ON -DVCPKG_BUILD_FUZZING=OFF -DVCPKG_EMBED_GIT_SHA=ON -DVCPKG_ADD_SOURCELINK=ON -DVCPKG_BASE_VERSION=%file-current-date%
+cmake -B %_vcpkg_tool_build% -S %_vcpkg_tool_source% -G "Visual Studio 16 2019" -A x64 -T v142 -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DVCPKG_DEVELOPMENT_WARNINGS=ON -DVCPKG_WARNINGS_AS_ERRORS=ON -DVCPKG_BUILD_FUZZING=OFF -DVCPKG_EMBED_GIT_SHA=ON -DVCPKG_ADD_SOURCELINK=ON -DVCPKG_BASE_VERSION=%_vcpkg-tool_latest_git-commit-date%
 
 cmake --build %_vcpkg_tool_build% --target vcpkg --clean-first --verbose --config release --parallel 8
 
