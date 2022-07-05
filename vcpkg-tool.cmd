2f2f49be07ef331e83c9bc253d220935bd18de60
General Jack O'Neill <68293825+StarGate-One@users.noreply.github.com>
2022-07-04 22:32:39 -0400

    Update CMAKE parameters

--- vcpkg-tool.cmd
+++ vcpkg-tool.cmd
@@ -5,6 +5,7 @@
 @set _root_drive=D:
 @set _vcpkg_root=%_root_drive%\vcpkg
 @set _vcpkg_exe=%_vcpkg_root%\vcpkg.exe
+@set _vcpkg_fmt_url=https://github.com/fmtlib/fmt/archive/refs/tags/8.1.1.tar.gz
 @set _vcpkg_metrics=%_vcpkg_root%\vcpkg.disable-metrics
 @set _vcpkg_tool=%_root_drive%\vcpkg-tool
 @rem set _vcpkg_tool_azure=%_vcpkg_tool%\azure-pipelines
@@ -14,7 +15,8 @@
 
 @call date-time.cmd
 
-@rem set _vcpkg-tool_ce_sha=EMPTY
+
+@set _vcpkg-tool_ce_sha=EMPTY
 @set _vcpkg-tool_latest_tag-refname-date=EMPTY
 
 @set _vcpkg_git_format="--format=%%cd"
@@ -26,13 +28,13 @@
 
 @cd %_vcpkg_tool%
 
-@rem set _vcpkg-tool_ce_sha equal to sha info in file located in ./azure-pipelines
-@rem for /f "tokens=* USEBACKQ" %%g in ("%_vcpkg_tool_azure%\vcpkg-ce-sha.txt") do (
-     @rem set _vcpkg-tool_ce_sha=%%g
-     @rem if %_vcpkg-tool_ce_sha% NEQ "EMPTY" (
-         @rem goto next1
-rem      )
-rem )
+@rem set _vcpkg-tool_ce_sha equal to sha on HEAD
+for /f "tokens=* USEBACKQ" %%g in (`git rev-parse HEAD`) do (
+     @set _vcpkg-tool_ce_sha=%%g
+     @if %_vcpkg-tool_ce_sha% NEQ "EMPTY" (
+         @goto next1
+     )
+)
 
 :next1
 @rem get date of last commit in CCYY-MM-DD format UTC Timezone
@@ -60,10 +62,32 @@ rem )
 )
 
 rem Build the vcpkg.exe source code
-cmake -B %_vcpkg_tool_build% -S %_vcpkg_tool_source% -G "Visual Studio 17 2022" -A x64 -T v143 -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DVCPKG_DEVELOPMENT_WARNINGS=ON -DVCPKG_WARNINGS_AS_ERRORS=ON -DVCPKG_BUILD_FUZZING=OFF -DVCPKG_BUILD_TLS12_DOWNLOADER=OFF -DVCPKG_EMBED_GIT_SHA=ON -DVCPKG_OFFICIAL_BUILD=OFF -DFETCHCONTENT_FULLY_DISCONNECTED=OFF -DVCPKG_ADD_SOURCELINK=OFF -DVCPKG_BASE_VERSION=%_vcpkg-tool_latest_tag-refname-date% -DVCPKG_VERSION=%_vcpkg-tool_latest_tag-refname-date% -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded 
+cmake -B %_vcpkg_tool_build% -S %_vcpkg_tool_source%                       ^
+      -G "Visual Studio 17 2022" -A x64 -T v143                            ^
+      -DBUILD_TESTING=OFF                                                  ^
+      -DCMAKE_BUILD_TYPE=Release                                           ^
+      -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded                           ^
+      -DVCPKG_BASE_VERSION=%_vcpkg-tool_latest_tag-refname-date%           ^
+      -DVCPKG_BUILD_FUZZING=OFF                                            ^
+      -DVCPKG_BUILD_TLS12_DOWNLOADER=ON                                    ^
+      -DVCPKG_CE_SHA=%_vcpkg-tool_ce_sha%                                  ^
+      -DVCPKG_DEVELOPMENT_WARNINGS=ON                                      ^
+      -DVCPKG_EMBED_GIT_SHA=ON                                             ^
+      -DVCPKG_FMT_URL=%_vcpkg_fmt_url%                                     ^
+      -DVCPKG_OFFICIAL_BUILD=ON                                            ^
+      -DVCPKG_STANDALONE_BUNDLE_SHA=%_vcpkg-tool_ce_sha%                   ^
+      -DVCPKG_VERSION=%_vcpkg-tool_ce_sha%                                 ^
+      -DVCPKG_WARNINGS_AS_ERRORS=ON 
+
+@rem goto end
 
 rem Compile/Link vcpkg.exe
-cmake --build %_vcpkg_tool_build% --target vcpkg --clean-first --verbose --config release --parallel 8
+cmake --build %_vcpkg_tool_build%  ^
+      --target vcpkg               ^
+      --clean-first                ^
+      --verbose                    ^
+      --config release             ^
+      --parallel 8 
 
 @if exist %_vcpkg_tool_exe% (
     @copy %_vcpkg_tool_exe% %_vcpkg_exe%
